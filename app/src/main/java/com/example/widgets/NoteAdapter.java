22be47514c2df116f5bcbbfe17e57335410a3160
b21mihpa <b21mihpa@student.his.se>
2022-04-12 11:39:25 +0200

    added: NoteAdapter

---
+++ app/src/main/java/com/example/widgets/NoteAdapter.java
@@ -0,0 +1,31 @@
+package com.example.widgets;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteAdapterViewHolder> {
+    @NonNull
+    @Override
+    public NoteAdapter.NoteAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteAdapter.NoteAdapterViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class NoteAdapterViewHolder extends RecyclerView.ViewHolder {
+        public NoteAdapterViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
